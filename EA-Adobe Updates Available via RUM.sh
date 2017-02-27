0a5bd4f2d63ff4b25490c5455b5490317b435488
John Mahlman <john.mahlman@gmail.com>
2017-02-27 14:39:56 -0500

    New EA for Adobe Updates
    Created new Casper extension attribute for letting us know if there are
    any Adobe updates using RUM.

---
+++ EA-Adobe Updates Available via RUM.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#
+# Created by John Mahlman, University of the Arts Philadelphia (jmahlman@uarts.edu)
+# Name: EA-Adobe Updates Available via RUM
+#
+# Purpose: Casper extension attribute for letting us know if there are any Adobe updates using RUM
+#
+# Changelog
+# 2/27/17 - EA created.
+#
+#
+rumlog=/var/tmp/RUMupdate.log
+rum=/usr/local/bin/RemoteUpdateManager
+
+# Not that it matters but we'll remove the old log file if it exists
+if [[ -f $rumlog ]] ; then
+	rm $rumlog
+fi
+
+#run RUM and output to the log file
+touch $rumlog
+$rum --action=list > $rumlog
+
+if [[ -f $rum ]]; then
+	if [[ "$(grep "Following Updates are applicable" $rumlog)" == "Following Updates are applicable on the system :" ]] ; then
+		result="Updates Available"
+	else
+		result="No Updates"
+	fi
+else
+	result="RUM not found"
+fi
+
+/bin/echo "<result>$result</result>"
